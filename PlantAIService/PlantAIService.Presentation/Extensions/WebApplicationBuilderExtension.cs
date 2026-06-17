6873e99a7b5b56c56310eacbb434243025203463
root <deniska1234denis@gamil.com>
2026-06-17 10:23:56 +0700

    added modules

---
+++ PlantAIService/PlantAIService.Presentation/Extensions/WebApplicationBuilderExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace PlantAIService.Extensions
+{
+    public static class WebApplicationBuilderExtension
+    {
+
+        public static void ImplementBuilderServices(this WebApplicationBuilder builder)
+        {
+
+            builder.Services.AddOpenApi();
+            builder.Services.AddGrpc();
+
+        }
+
+    }
+}
