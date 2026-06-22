035892812dbf329ffe054b5630a73ac3533df1b4
Денис <kheragacy@MacBook-Air-Denis.local>
2026-06-23 04:50:04 +0700

    add migration

---
+++ TelemetryService/Infrastructure/Migrations/20260622075505_edit models.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class editmodels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "SensorType",
+                columns: new[] { "Id", "TypeName" },
+                values: new object[] { 3, "CO2" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "SensorType",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
